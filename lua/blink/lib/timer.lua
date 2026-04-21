e5678fe566e86553403b3129a3684389c84fafb5
Liam Dyer <liamcdyer@gmail.com>
2026-04-20 20:27:55 -0400

    fix(timer): stack overflow and simplify

--- lua/blink/lib/timer.lua
+++ lua/blink/lib/timer.lua
@@ -1,32 +1,3 @@
---- @class blink.lib.Timer
---- @field internal uv_timer_t
---- @field cancel_id integer
-local timer = {}
-
---- @param timeout integer
---- @param repeat_n integer
---- @param callback fun()
---- @return 0|nil success, string? err_name, string? err_msg
-function timer:start(timeout, repeat_n, callback)
-  local id = self.cancel_id
-
-  return self.internal:start(
-    timeout,
-    repeat_n,
-    vim.schedule_wrap(function()
-      if id ~= self.cancel_id then return end
-      callback()
-    end)
-  )
-end
-
-function timer:stop()
-  self.cancel_id = self.cancel_id + 1
-  return self.internal:stop()
-end
-
---------------------
-
 --- @class blink.lib.timer
 local M = {}
 
@@ -34,22 +5,35 @@ local M = {}
 ---
 --- Unlike the built-in `vim.uv.new_timer()`, this timer will automatically schedule callbacks,
 --- and handle cancellation without racing.
---- @return blink.lib.Timer
+--- @return uv.uv_timer_t
 function M.new()
-  local self = {
-    -- whenever the timer is cancelled, this id is incremented
-    cancel_id = 0,
-    internal = vim.uv.new_timer(),
-  }
+  local timer = vim.uv.new_timer()
 
-  return setmetatable(self, {
-    __index = function(_, key)
-      if key == 'internal' or key == 'start' or key == 'stop' then return self[key] end
-      return self.internal[key]
+  -- whenever the timer is cancelled, this id is incremented
+  local cancel_id = 0
+
+  return setmetatable({
+    start = function(self, timeout, repeat_n, callback)
+      local id = cancel_id
+
+      return timer:start(
+        timeout,
+        repeat_n,
+        vim.schedule_wrap(function()
+          if id ~= cancel_id then return end
+          callback()
+        end)
+      )
     end,
-    __newindex = function(_, key, value)
-      if key ~= 'cancel_id' then error('Cannot set field ' .. key) end
-      self.cancel_id = value
+
+    stop = function(self)
+      cancel_id = cancel_id + 1
+      return timer:stop()
+    end,
+  }, {
+    __index = function(self, key)
+      if key == 'start' or key == 'stop' then return self[key] end
+      return timer[key]
     end,
   })
 end
